bdf3953f317d5d20f3a8657ffb22c839bc4f0001
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-10-11 08:52:20 -0300

    Conectando front e back com HelloResource, index.html e script.js teste.

---
+++ src/main/java/br/com/facilitareabi/config/AppConfig.java
@@ -0,0 +1,12 @@
+package br.com.facilitareabi.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/api")
+public class AppConfig extends ResourceConfig {
+
+    public AppConfig(){
+        packages("br.com.facilitareabi.resource");
+    }
+}
--- src/main/webapp/teste.jsp
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
- <title> FacilitaReabi </title>
-</head>
-<body>
-<h1>Testando: Tudo que estiver dentro de webapp é visível pelo browser</h1>
-<h2>Configuração para distinguir aplicações</h2>
-</body>
-</html>
